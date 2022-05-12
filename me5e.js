844c8ec9a08db60432218f17df7a2612d00eef07
Jacob Lucas <jtljac2@gmail.com>
2022-05-12 01:43:57 +0100

    Fully removed honour and sanity

--- me5e.js
+++ me5e.js
@@ -122,16 +122,6 @@ Hooks.once("init", function() {
   // Register System Settings
   registerSystemSettings();
 
-  // Remove honor & sanity from configuration if they aren't enabled
-  if ( !game.settings.get("me5e", "honorScore") ) {
-    delete ME5E.abilities.hon;
-    delete ME5E.abilityAbbreviations.hon;
-  }
-  if ( !game.settings.get("me5e", "sanityScore") ) {
-    delete ME5E.abilities.san;
-    delete ME5E.abilityAbbreviations.san;
-  }
-
   // Patch Core Functions
   CONFIG.Combat.initiative.formula = "1d20 + @attributes.init.mod + @attributes.init.prof + @attributes.init.bonus + @abilities.dex.bonuses.check + @bonuses.abilities.check";
   Combatant.prototype._getInitiativeFormula = _getInitiativeFormula;
