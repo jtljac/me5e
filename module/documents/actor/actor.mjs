73045002dafb2b90697e78a3d1a0630685e78b8c
Jacob Lucas <jtljac2@gmail.com>
2022-08-31 21:20:13 +0100

    Moved initiative calculation to actor

--- module/documents/actor/actor.mjs
+++ module/documents/actor/actor.mjs
@@ -5,6 +5,7 @@ import ProficiencySelector from "../../applications/proficiency-selector.mjs";
 import Item5e from "../item.mjs";
 import {Rule5e} from "../../rules/_module.mjs";
 import SelectItemsPrompt from "../../applications/select-items-prompt.mjs";
+
 /**
  * Extend the base Actor class to implement additional system-specific logic.
  */
@@ -473,6 +474,46 @@ export default class Actor5e extends Actor {
   /* -------------------------------------------- */
   /*  Gameplay Mechanics                          */
   /* -------------------------------------------- */
+
+  /**
+   * Get a formula for the initiative of the actor
+   * See Combat._getInitiativeFormula for more detail.
+   * @returns {string}  Final initiative formula for the actor.
+   */
+  getInitiativeFormula() {
+    /*
+     * Apply advantage, proficiency, or bonuses where appropriate
+     * Apply the dexterity score as a decimal tiebreaker if requested
+     */
+    const init = this.system.attributes.init;
+    const rollData = this.getRollData();
+
+    // Construct initiative formula parts
+    let nd = 1;
+    let mods = "";
+    if ( this.getFlag("me5e", "halflingLucky") ) mods += "r1=1";
+    if ( this.getFlag("me5e", "initiativeAdv") ) {
+      nd = 2;
+      mods += "kh";
+    }
+    const parts = [
+      `${nd}d20${mods}`,
+      init.mod,
+      (init.prof.term !== "0") ? init.prof.term : null,
+      (init.value !== 0) ? init.value : null
+    ];
+
+    // Ability Check Bonuses
+    const checkBonus = this.system.abilities[init.ability]?.bonuses?.check;
+    const globalCheckBonus = this.system.bonuses?.abilities?.check;
+    if ( checkBonus ) parts.push(Roll.replaceFormulaData(checkBonus, rollData));
+    if ( globalCheckBonus ) parts.push(Roll.replaceFormulaData(globalCheckBonus, rollData));
+
+    // Optionally apply Dexterity tiebreaker
+    const tiebreaker = game.settings.get("me5e", "initiativeDexTiebreaker");
+    if ( tiebreaker ) parts.push((this.system.abilities.dex?.value ?? 0) / 100);
+    return parts.filter(p => p !== null).join(" + ");
+  }
 
   /** @override */
   async modifyTokenAttribute(attribute, value, isDelta, isBar) {
--- module/documents/combat.mjs
+++ module/documents/combat.mjs
@@ -1,39 +1,12 @@
 /**
  * Override the default Initiative formula to customize special behaviors of the system.
- * Apply advantage, proficiency, or bonuses where appropriate
- * Apply the dexterity score as a decimal tiebreaker if requested
+ * Passes the responsibility onto the actor
  * See Combat._getInitiativeFormula for more detail.
  * @returns {string}  Final initiative formula for the actor.
  */
 export function _getInitiativeFormula() {
   const actor = this.actor;
   if ( !actor ) return "1d20";
-  const init = actor.system.attributes.init;
-  const rollData = actor.getRollData();
 
-  // Construct initiative formula parts
-  let nd = 1;
-  let mods = "";
-  if ( actor.getFlag("me5e", "halflingLucky") ) mods += "r1=1";
-  if ( actor.getFlag("me5e", "initiativeAdv") ) {
-    nd = 2;
-    mods += "kh";
-  }
-  const parts = [
-    `${nd}d20${mods}`,
-    init.mod,
-    (init.prof.term !== "0") ? init.prof.term : null,
-    (init.value !== 0) ? init.value : null
-  ];
-
-  // Ability Check Bonuses
-  const checkBonus = actor.system.abilities[init.ability]?.bonuses?.check;
-  const globalCheckBonus = actor.system.bonuses?.abilities?.check;
-  if ( checkBonus ) parts.push(Roll.replaceFormulaData(checkBonus, rollData));
-  if ( globalCheckBonus ) parts.push(Roll.replaceFormulaData(globalCheckBonus, rollData));
-
-  // Optionally apply Dexterity tiebreaker
-  const tiebreaker = game.settings.get("me5e", "initiativeDexTiebreaker");
-  if ( tiebreaker ) parts.push((actor.system.abilities.dex?.value ?? 0) / 100);
-  return parts.filter(p => p !== null).join(" + ");
+  return actor.getInitiativeFormula()
 }
